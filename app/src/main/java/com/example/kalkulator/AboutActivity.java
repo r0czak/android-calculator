c5b03a32cce9767f9fcba7eb67f50e3e9d0ef31d
Bartosz Kaczor <222453@edu.p.lodz.pl>
2021-04-30 00:38:46 +0200

    Add Simple and Advanced Calculator Layout

--- app/src/main/java/com/example/kalkulator/AboutActivity.java
+++ app/src/main/java/com/example/kalkulator/AboutActivity.java
@@ -1,13 +1,29 @@
 package com.example.kalkulator;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class AboutActivity extends AppCompatActivity {
+
+  private Button backButton;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_about);
+
+    backButton = findViewById(R.id.buttonBackId);
+
+    backButton.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(i);
+          }
+        });
   }
 }
--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.kalkulator;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,21 +8,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-  private Button buttonSimpleId;
-  private Button buttonAdvId;
-  private Button buttonAboutId;
-  private Button buttonExitId;
+  private Button buttonSimple;
+  private Button buttonAdv;
+  private Button buttonAbout;
+  private Button buttonExit;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    buttonAboutId.setOnClickListener(new View.OnClickListener() {
-      @Override
-      public void onClick(View v) {
-        
-      }
-    });
+    buttonSimple = findViewById(R.id.buttonSimpleId);
+    buttonAdv = findViewById(R.id.buttonAdvId);
+    buttonAbout = findViewById(R.id.buttonAboutId);
+    buttonExit = findViewById(R.id.buttonExitId);
+
+    buttonAbout.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), AboutActivity.class);
+            startActivity(i);
+          }
+        });
+
+    buttonSimple.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), SimpleCalculatorActivity.class);
+            startActivity(i);
+          }
+        });
+
+    buttonAdv.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            Intent i = new Intent(getApplicationContext(), AdvancedCalculatorActivity.class);
+            startActivity(i);
+          }
+        });
+    buttonExit.setOnClickListener(
+        new View.OnClickListener() {
+          @Override
+          public void onClick(View v) {
+            moveTaskToBack(true);
+            android.os.Process.killProcess(android.os.Process.myPid());
+            System.exit(1);
+          }
+        });
   }
 }
